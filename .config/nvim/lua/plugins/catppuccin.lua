85d964a2985a4c39a56eb778875615705c2b9ff6
Long Nguyen-Vu <longnv.work@gmail.com>
2025-02-04 10:54:19 +0900

    Add nvim
    Signed-off-by: Long Nguyen-Vu <longnv.work@gmail.com>

---
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,28 @@
+return {
+{
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    opts = {
+    flavour = "mocha", -- Available flavours: latte, frappe, macchiato, mocha
+    background = { dark = "mocha" },
+    transparent_background = false,
+    term_colors = true,
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        notify = true,
+        mini = true,
+        -- For more plugins integrations see https://github.com/catppuccin/nvim#integrations
+    },
+    },
+    config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
+    end,
+},
+}
+
