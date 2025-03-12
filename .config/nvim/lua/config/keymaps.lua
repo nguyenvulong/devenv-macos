f78bbd62e16c5342944b79e362b2c3f96cd669c6
Long Nguyen-Vu <longnv.work@gmail.com>
2025-03-12 12:20:08 +0900

    Update keymap to exit terminal mode
    Signed-off-by: Long Nguyen-Vu <longnv.work@gmail.com>

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Map <Esc> to exit terminal-mode
+vim.api.nvim_set_keymap("t", "<C-x>", "<C-\\><C-n>", { noremap = true, silent = true })
