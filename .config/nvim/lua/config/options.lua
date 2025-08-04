59639cdc0b392e36f885a696f507ee7b9ff3ccd7
Long Nguyen-Vu <1311412+nguyenvulong@users.noreply.github.com>
2025-08-04 16:00:28 +0900

    add clipboard.osc52
    Signed-off-by: Long Nguyen-Vu <1311412+nguyenvulong@users.noreply.github.com>

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -12,3 +12,17 @@ vim.g.lazyvim_python_ruff = "ruff"
 -- only for diagnostics. The rest of LSP support will still be
 -- provided by rust-analyzer.
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
